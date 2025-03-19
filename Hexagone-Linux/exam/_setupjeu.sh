8789c9960e4351c8249c4c4cdd72fc3992bbde22
Den <eden.malhis@gmail.com>
2025-03-19 23:37:33 +0100

    Update Fléchettes

---
+++ Hexagone-Linux/exam/_setupjeu.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+DB_FILE="players.txt"
+SEP=";"
+
+menu() {
+	read -p "Select one of the following options : 501 double out (501) ou 301 double out (301) or exit this menu (Q) : " game_choice
+	case $game_choice in
+	501)
+		value="501"
+		echo $value
+		;;
+	301)
+		valeur="301"
+		echo $valeur
+		;;
+	Q)
+		echo "Goodbye" && exit 0
+		;;
+	*)
+		echo "The choices are 501, 301 or Q to exit"
+		;;
+	esac
+}
+
+how_m_players() {
+	read -p "How many players ? " nb_players
+	echo $nb_players
+}
+
+get_next_id() {
+	next_id=$(awk -F ";" '{print $1}' "$DB_FILE" | sort -n | tail -1)
+	next_id=$((next_id + 1))
+	echo $next_id
+}
+
+create() {
+	echo $1
+
+}
+
+show_scores() {
+	for ((i = 1; i <= nb; i++)); do
+		echo "${joueurs[$i]} = ${scores[$i]}"
+	done
+}
+end_game() {
+	>players.txt
+}
---
+++ Hexagone-Linux/exam/main.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+. _setupjeu.sh
+
+main() {
+    nb=$(how_m_players)
+    echo "Okay, I'll setup the game for $nb players."
+    game=$(menu)
+    create $game
+    joueurs=()
+    scores=()
+    for ((i = 1; i <= nb; i++)); do
+        echo "Creating a new player"
+        read -p "First name : " first_name
+        echo "You'll be the Player $i"
+        joueurs[$i]="$first_name"
+        scores[$i]=$game
+        echo "${joueurs[$i]} added succesfully."
+    done
+    count=0
+    while [ $count -eq 0 ]; do
+        for ((y = 1; y <= nb; y++)); do
+            show_scores
+            echo "Player $y, your turn !"
+            echo "Enter your score : "
+            read points
+            while [ $points -gt 180 ]; do
+                echo "I need a valid number(less than 180), try again."
+                echo "Enter your score :"
+                read points
+            done
+            if [ ${scores[$y]} -le 10 ] && [ $points -ne ${scores[$y]} ]; then
+                echo "You need to reach exactly 0, try again !"
+                continue
+            else
+                newscore=$((${scores[$y]} - $points))
+                scores[$y]=$newscore
+                if ((${scores[$y]} < 1)); then
+                    echo "Player $y has won !"
+                    count=1
+                    break
+                fi
+            fi
+            show_scores
+        done
+    done
+}
+
+main
+
+end_game
